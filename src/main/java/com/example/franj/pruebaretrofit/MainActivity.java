ec8b49acb30bf985d1dc22cc8c80476e1d8b90d0
Javier Arellano <franjarca@hotmail.com>
2017-11-15 10:38:35 +0100

    prueba

--- src/main/java/com/example/franj/pruebaretrofit/MainActivity.java
+++ src/main/java/com/example/franj/pruebaretrofit/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.franj.pruebaretrofit;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TabHost;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
@@ -24,6 +26,7 @@ public class MainActivity extends AppCompatActivity implements Callback<Chiste> 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        creartab();
         texto1 = (TextView) findViewById(R.id.texto1);
         botonPedir = (Button) findViewById(R.id.botonPedir);
         botonPedir.setOnClickListener(new View.OnClickListener() {
@@ -32,6 +35,27 @@ public class MainActivity extends AppCompatActivity implements Callback<Chiste> 
                 start();
             }
         });
+    }
+
+    public void creartab(){
+        Resources res = getResources();
+
+        TabHost tabs=(TabHost)findViewById(android.R.id.tabhost);
+        tabs.setup();
+
+        TabHost.TabSpec spec=tabs.newTabSpec("mitab1");
+        spec.setContent(R.id.tab1);
+        spec.setIndicator("TAB1",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
+        spec=tabs.newTabSpec("mitab2");
+        spec.setContent(R.id.tab2);
+        spec.setIndicator("TAB2",
+                res.getDrawable(android.R.drawable.ic_dialog_map));
+        tabs.addTab(spec);
+
+        tabs.setCurrentTab(0);
     }
 
     public void start() {
